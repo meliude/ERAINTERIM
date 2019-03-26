61c97d0bb01f388b7d112300d0c6bb8c4d3c7636
meliude <meliudesantos@gmail.com>
2019-03-26 15:30:13 -0300

    modified:   pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -19,16 +19,108 @@ server.retrieve({
     "target": "output",
 })
 
-file = '_mars-atls12-1998to2008.grib'
-data = pygrib.open(file)
-dp = data.select(name = '2 metre dewpoint temperature')
-tp = data.select(name = 'Total precipitation')
+import pygrib
+import pandas as pd
 
 
-#jan=0:31, 365:396, 730:761, 1096:1127, 1461:1492, 1826:1857, 2191:2222, 2557:2588, 2922:2953,3287:3318,3652:3683
-#feb=31:59, 396:424, 761:790, 1127:1155, 1492:1520, 1857:1885, 2222:2251, 2588:2616, 2953:2981, 3318:3346, 3683:3712
-#mar=59:90, 424:455, 790:821, 1155:1186, 1520:1551, 1885:1916, 2251:2282, 2616:2647, 2981:3012, 3346:3377, 3712:3743
-#apr=90:120, 455:485, 821:851, 1186:1216, 1551:1581, 1916:1946, 2282:2312, 2647:2677, 3012:3042, 3377:3407, 3743:3773
-#may=120:151, 485:516, 851:882, 1216:1247, 1581:1612, 1946:1977, 2312:2343, 2677:2708, 3042:3073, 3407:3438, 3773:3804
-#jun=151:181, 516:546, 882:912, 1247:1277, 1612:1642, 1977:2007, 2343:2373, 2708:2738, 3073:3103, 3438:3468, 3804:3834
-#jul=181:212, 546:577, 912:943, 1277:1308, 1642:1673, 2007:2038, 2373:2404, 2738:2769, 3103:3134, 3468:3499, 3834:3865
+years = ['1998.grib', '1999.grib', '2000.grib', '2001.grib', '2002.grib', '2003.grib', '2004.grib', '2005.grib', '2006.grib', '2007.grib', '2008.grib']
+data = []
+dp = [] #dewpoint
+tp = [] #totalprecipitation
+
+for i in range(0,11):
+    data.append(pygrib.open(years[i]))
+    dp.append(data[i].select(name = '2 metre dewpoint temperature'))
+    tp.append(data[i].select(name = 'Total precipitation'))
+
+#leapyears: 2000, 2004 and 2008
+##jan(0,31),feb(31,60),mar(60,91),apr(91,121),may(121,152),jun(152,182),jul(182,213),aug(213,244)
+##sep(244,274),oct(274,305),nov(305,335),dec(335,366)
+
+#for dewpoint temperature
+jan_dp,feb_dp,mar_dp,apr_dp,may_dp,jun_dp,jul_dp,aug_dp,sep_dp,oct_dp,nov_dp,dec_dp=[], [], [], [], [], [], [], [], [], [], [], []
+
+#convert Kelvin to Celsius
+def convert(x):
+    return x-273.15
+
+for i in range(0,11):
+    if i == (2,6,10):
+        for j in range(0,31):
+            jan_dp.append(convert(dp[i][j].values))
+        for j in range(31,60):
+            feb_dp.append(convert(dp[i][j].values))
+        for j in range(60,91):
+            mar_dp.append(convert(dp[i][j].values))
+        for j in range(91,121):
+            apr_dp.append(convert(dp[i][j].values))
+        for j in range(121,152):
+            may_dp.append(convert(dp[i][j].values))
+        for j in range(152,182):
+            jun_dp.append(convert(dp[i][j].values))
+        for j in range(182,213):
+            jul_dp.append(convert(dp[i][j].values))
+        for j in range(213,244):
+            aug_dp.append(convert(dp[i][j].values))
+        for j in range(244,274):
+            sep_dp.append(convert(dp[i][j].values))
+        for j in range(274,305):
+            oct_dp.append(convert(dp[i][j].values))
+        for j in range(305,335):
+            nov_dp.append(convert(dp[i][j].values))
+        for j in range(335,366):
+            dec_dp.append(convert(dp[i][j].values))
+    if i != (2,6,10):
+        for j in range(0,31):
+            jan_dp.append(convert(dp[i][j].values))
+        for j in range(31,59):
+            feb_dp.append(convert(dp[i][j].values))
+        for j in range(59,90):
+            mar_dp.append(convert(dp[i][j].values))
+        for j in range(90,120):
+            apr_dp.append(convert(dp[i][j].values))
+        for j in range(120,151):
+            may_dp.append(convert(dp[i][j].values))
+        for j in range(151,181):
+            jun_dp.append(convert(dp[i][j].values))
+        for j in range(181,212):
+            jul_dp.append(convert(dp[i][j].values))
+        for j in range(212,243):
+            aug_dp.append(convert(dp[i][j].values))
+        for j in range(243,273):
+            sep_dp.append(convert(dp[i][j].values))
+        for j in range(273,304):
+            oct_dp.append(convert(dp[i][j].values))
+        for j in range(304,334):
+            nov_dp.append(convert(dp[i][j].values))
+        for j in range(334,365):
+            dec_dp.append(convert(dp[i][j].values))
+
+#jan,mar,may,jul,aug,oct,dec
+
+def totalsum(x,a=1,b=2):
+    return sum(x[a],x[b])
+
+##0,30,31,61,62,92,93,123,124,154,155,185,186,216,217,247,248,278,279,309,310,340
+lb=[0,31,62,93,124,155,186,217,248,279,310]
+up=[30,61,92,123,154,185,216,247,278,309,340]
+
+jan_tdp, mar_tdp, may_tdp, jul_tdp, aug_tdp,oct_tdp,dec_tdp=[], [], [], [], [], [], []
+for i,j in zip(lb,up):
+    jan_tdp.append(totalsum(jan_dp,a=i,b=j))
+    mar_tdp.append(totalsum(mar_dp,a=i,b=j))
+    may_tdp.append(totalsum(may_dp,a=i,b=j))
+    jul_tdp.append(totalsum(jul_dp,a=i,b=j))
+    aug_tdp.append(totalsum(aug_dp,a=i,b=j))
+    oct_tdp.append(totalsum(oct_dp,a=i,b=j))
+    dec_tdp.append(totalsum(dec_dp,a=i,b=j))
+
+jan_tdp, mar_tdp, may_tdp, jul_tdp, aug_tdp,oct_tdp,dec_tdp=sum(jan_tdp)/11, sum(mar_tdp)/11, sum(may_tdp)/11, sum(jul_tdp)/11, sum(aug_tdp)/11, sum(oct_tdp)/11, sum(dec_tdp)/11
+
+pd.DataFrame(jan_tdp).to_csv('grid_jan.csv')
+pd.DataFrame(may_tdp).to_csv('grid_may.csv')
+pd.DataFrame(jul_tdp).to_csv('grid_jul.csv')
+pd.DataFrame(aug_tdp).to_csv('grid_aug.csv')
+pd.DataFrame(oct_tdp).to_csv('grid_oct.csv')
+pd.DataFrame(dec_tdp).to_csv('grid_dec.csv')
+
